4b4ae1ea7a4dd820d6c33df1b4fb660917b6d41d
hyruleismyrule <hyruleismyrule@gmail.com>
2022-10-22 16:18:58 -0400

    rest ready

--- validation.js
+++ validation.js
@@ -5,13 +5,10 @@ exports.characterValidation = [
     check('rarity', 'Rarity should the number of stars, ex. 5').not().isEmpty().isNumeric().trim().escape(),
     check('weapon', 'Weapon is requied').not().isEmpty().trim().escape(),
     check('vision', 'Vision is requied').not().isEmpty().trim().escape(),
-    check('birthday', 'Birthday is requied').not().isEmpty().trim().escape(),
-    // constellations
-    check('constellations', 'Constellations is requied').not().isEmpty().trim().escape(),
+    check('birthday', 'Birthday is requied').not().isEmpty().trim(),
     check('modelType', 'Model Type is requied').not().isEmpty().trim().escape(),
     check('specialDish', 'Special Dish is requied').not().isEmpty().trim().escape(),
     check('Region', 'Region is requied').not().isEmpty().trim().escape(),
-    check('talents', 'Talents is requied').not().isEmpty().trim().escape(),
 ]
 
 
