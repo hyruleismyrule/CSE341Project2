7864ecf6c4a2c77a071679645755c66229fe53d1
hyruleismyrule <hyruleismyrule@gmail.com>
2022-10-29 00:46:56 -0400

    auth still breaking on sending code

--- controllers/authorization.controller.js
+++ controllers/authorization.controller.js
@@ -3,13 +3,35 @@ const clientSecret = process.env.CLIENT_SECRET;
 const redirectURL = process.env.REDIRECT_URL;
 const authorizationHost = process.env.AUTHORIZATION_HOST;
 
-const authorizationURL = authorizationHost + "/authorize?response_type=code&client_id=" + clientID + "&redirect_uri=" + redirectURL + "&scope=openid%20profile%20email&state=1234";
+const authorizationURL = authorizationHost + "/authorize?response_type=code&client_id=" + clientID + "&redirect_uri=" + encodeURIComponent(redirectURL) + "&scope=openid%20profile%20email&state=1234";
+const tokenURL = authorizationHost + "/oauth/token/";
 
 const AuthorizationController = {
     login: (req, res) => {
         // res.send("login endpoint");
         // res.redirect("https://dev-6rfoxiaajiqencck.us.auth0.com/authorize?response_type=code&client_id=f2SSSB3Z4g6jwPxkRAVL1Mx9gajH9qCe&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fcallback&scope=openid%20profile%20email&state=1234");
         res.redirect(authorizationURL);
+    },
+    callback: async (req, res) => {
+        // res.json(req.query.code);
+        const responce = await fetch(tokenURL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: new URLSearchParams( {
+                grant_type: "authorization_code",
+                client_id: clientID,
+                client_secret: clientSecret,
+                redirect_uri: redirectURL,
+                scope: "openid profile email",
+                code: req.query.code
+            })
+        });
+
+        const jsonResponce = await responce.json();
+
+        res.json(jsonResponce);
     }
 }
 
--- routes/authorization.js
+++ routes/authorization.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const AuthorizationController = require("../controllers/authorization.controller");
 
 router.get("/login", AuthorizationController.login);
+router.get("/callback", AuthorizationController.callback);
 
 module.exports = router;
